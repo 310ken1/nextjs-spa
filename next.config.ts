da355535b210e8bb6e68b0f3f09ffaa1212222a9
Kenichi Sato <310dev@gmail.com>
2024-11-30 21:24:05 +0000

    Static Exportsに関するコメント追加

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Static Exports
+  // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
   output: "export", // SSG設定
 };
 
