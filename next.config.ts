df23151fb8fc6fb9126a15a78322ec8be74c1c91
Kenichi Sato <310dev@gmail.com>
2024-12-11 15:47:35 +0000

    DevContainerで、HotReloadが効かない問題の対応

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Static Exports
   // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-  output: "export", // SSG設定
+  output: "export",
+  // Hot reload in docker
+  // https://github.com/vercel/next.js/issues/36774
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
