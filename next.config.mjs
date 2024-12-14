1710cdb5c305b70f372c1d944e16b2a671400205
Kenichi Sato <310dev@gmail.com>
2024-12-14 05:17:21 +0000

    Next.js14に変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Static Exports
   // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
   output: "export",
