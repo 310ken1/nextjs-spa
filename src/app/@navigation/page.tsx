c85686c0ee58aa0153ee388b8f6e02cf1b7747f6
Kenichi Sato <310dev@gmail.com>
2024-12-18 17:57:19 +0000

    ナビゲーションバーにボタンを配置

--- src/app/@navigation/page.tsx
+++ src/app/@navigation/page.tsx
@@ -1,11 +1,22 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import styles from "./navigation.module.css";
+import { Button } from "react-bootstrap";
 
 export default function Navigation() {
   return (
     <div className={styles.navigation}>
-      <button className={`bi bi-house ${styles.button_icon}`}></button>
-      <button className={`bi bi-gear ${styles.button_icon}`}></button>
+      <div className={styles.top_area}>
+        <Button
+          className={`bi bi-house ${styles.button}`}
+          variant="light"
+        ></Button>
+      </div>
+      <div className={styles.bottom_area}>
+        <Button
+          className={`bi bi-gear ${styles.button}`}
+          variant="light"
+        ></Button>
+      </div>
     </div>
   );
 }
