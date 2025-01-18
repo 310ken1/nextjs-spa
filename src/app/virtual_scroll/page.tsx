71562c8d5606472efd59034347c7d33257606d0d
Kenichi Sato <310dev@gmail.com>
2025-01-19 03:03:55 +0900

    TanStack Virtualが正しく表示されない問題を修正

--- src/app/virtual_scroll/page.tsx
+++ src/app/virtual_scroll/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 
-import { useWeatherData } from "@/lib/open_meteo_hooks";
+import { TemperatureHistory, useWeatherData } from "@/lib/open_meteo_hooks";
 import React from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  Row,
+  useReactTable,
+} from "@tanstack/react-table";
+
 import { useVirtualizer } from "@tanstack/react-virtual";
 
 export default function WeatherPage() {
@@ -9,13 +18,42 @@ export default function WeatherPage() {
   endDate.setDate(endDate.getDate());
   const startDate = new Date();
   startDate.setFullYear(endDate.getFullYear() - 1);
-  const { data } = useWeatherData(35.6895, 139.6917, startDate, endDate);
+  let { data } = useWeatherData(35.6895, 139.6917, startDate, endDate);
+
+  const columns = React.useMemo<ColumnDef<TemperatureHistory>[]>(
+    () => [
+      {
+        accessorKey: "time",
+        header: "日時",
+      },
+      {
+        accessorKey: "temperature",
+        header: "気温",
+      },
+    ],
+    []
+  );
+  if (!data) data = [];
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
+  });
+  const { rows } = table.getRowModel();
 
   const parentRef = React.useRef<HTMLDivElement>(null);
   const rowVirtualizer = useVirtualizer({
-    count: data?.length || 0,
+    count: rows.length,
     getScrollElement: () => parentRef.current,
     estimateSize: () => 25,
+    measureElement:
+      typeof window !== "undefined" &&
+      navigator.userAgent.indexOf("Firefox") === -1
+        ? (element) => element?.getBoundingClientRect().height
+        : undefined,
+    overscan: 5,
   });
   return (
     <>
@@ -23,43 +61,100 @@ export default function WeatherPage() {
       <div
         ref={parentRef}
         style={{
-          display: "block",
-          width: "100%",
-          height: "100%",
           overflow: "auto",
+          position: "relative",
+          height: "100%",
         }}
       >
-        <table
-          style={{
-            position: "relative",
-            height: `${rowVirtualizer.getTotalSize()}px`,
-          }}
-        >
-          <thead>
-            <tr>
-              <th>インデックス</th>
-              <th>日時</th>
-              <th>気温</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rowVirtualizer.getVirtualItems().map((virtualRow) => (
+        <table style={{ display: "grid" }}>
+          <thead
+            style={{
+              display: "grid",
+              position: "sticky",
+              top: 0,
+              zIndex: 1,
+              backgroundColor: "white",
+            }}
+          >
+            {table.getHeaderGroups().map((headerGroup) => (
               <tr
-                key={virtualRow.key}
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  width: "100%",
-                  height: `${virtualRow.size}px`,
-                  transform: `translateY(${virtualRow.start}px)`,
-                }}
+                key={headerGroup.id}
+                style={{ display: "flex", width: "100%" }}
               >
-                <td>{virtualRow.index}</td>
-                <td>{data![virtualRow.index][0]}</td>
-                <td>{data![virtualRow.index][1]}</td>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th
+                      key={header.id}
+                      style={{
+                        display: "flex",
+                        width: header.getSize(),
+                        border: "1px solid black",
+                      }}
+                    >
+                      <div
+                        {...{
+                          className: header.column.getCanSort()
+                            ? "cursor-pointer select-none"
+                            : "",
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {{
+                          asc: " 🔼",
+                          desc: " 🔽",
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                    </th>
+                  );
+                })}
               </tr>
             ))}
+          </thead>
+          <tbody
+            style={{
+              display: "grid",
+              height: `${rowVirtualizer.getTotalSize()}px`, //tells scrollbar how big the table is
+              position: "relative", //needed for absolute positioning of rows
+            }}
+          >
+            {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+              const row = rows[virtualRow.index] as Row<TemperatureHistory>;
+              return (
+                <tr
+                  data-index={virtualRow.index}
+                  ref={(node) => rowVirtualizer.measureElement(node)}
+                  key={row.id}
+                  style={{
+                    display: "flex",
+                    position: "absolute",
+                    transform: `translateY(${virtualRow.start}px)`,
+                    width: "100%",
+                  }}
+                >
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td
+                        key={cell.id}
+                        style={{
+                          display: "flex",
+                          width: cell.column.getSize(),
+                          border: "1px solid black",
+                        }}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
